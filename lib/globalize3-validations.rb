a28f218b8d7283dd26b97617dddf5d13a4ca6134
mherold <herold@emjot.de>
2013-03-21 14:25:16 +0100

    Add missing dependency and fix load order

--- lib/globalize3-validations.rb
+++ lib/globalize3-validations.rb
@@ -1,5 +1,7 @@
 require "globalize3/validations/version"
-require "active_record/validations/globalized_uniqueness"
 
 require "active_support"
 require "active_record"
+require "globalize3"
+
+require "active_record/validations/globalized_uniqueness"
